6e2f445c8734919e2dd0c1d008c243057f4a5dc0
kim3489 <kcw5608@naver.com>
2024-11-22 01:40:52 +0900

    deploy

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@
 //
 // })
 
-
-module.exports = {
-  publicPath: 'https://kim3489.github.io/vue-assignment.github.io/',
-  outputDir: 'dist'
-}
+//
+// module.exports = {
+//   publicPath: 'https://kim3489.github.io/vue-assignment.github.io/',
+//   outputDir: 'dist'
+// }
