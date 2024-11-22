0548053bae2e6764f2b9e1734a78309139181b60
kim3489 <kcw5608@naver.com>
2024-11-22 10:07:02 +0900

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-    publicPath: 'vue-assignment.github.io/vue',
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//     publicPath: 'vue-assignment.github.io',
+//
+// })
 
-})
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/vue-assignment.github.io/'
+        : '/'
+}
