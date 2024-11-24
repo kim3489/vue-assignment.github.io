7e8460bfcf0ead9e3452d16569a370d01c10cb07
kim3489 <kcw5608@naver.com>
2024-11-24 19:55:10 +0900

    update baseurl

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,6 @@ import axios from "axios";
 library.add(faUserSecret)
 
 axios.defaults.baseURL = 'https://kim3489.github.io/vue-assignment.github.io/'
-App.config.globalProperties.axios.axios;
 createApp(App).use(router)
-    .config.globalProperties.axios
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
