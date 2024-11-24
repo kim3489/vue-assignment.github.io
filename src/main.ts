953fa25578a6da998e8c6e4a469004dde0499b1b
kim3489 <kcw5608@naver.com>
2024-11-24 19:58:30 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import "@/assets/css/tailwind.css";
-import axios from "axios";
 library.add(faUserSecret)
 
-axios.defaults.baseURL = 'https://kim3489.github.io/vue-assignment.github.io/'
 createApp(App).use(router)
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
