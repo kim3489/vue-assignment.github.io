e27582d3783c7d25e73629828bef6c68834f8fdd
kim3489 <kcw5608@naver.com>
2024-11-27 18:51:54 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/vue-assignment.github.io.git/'
-      : '/',
-  outputDir: 'docs'
+  publicPath: '/vue-assignment.github.io/',
 })
